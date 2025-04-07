514dc1019ebe369767df917ed9bc1265c068f41f
thirumalayeruva <thirumalareddyyeeruva@gmailcom>
2025-04-07 10:34:50 +0530

    report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here (if needed)
     },
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"  // 🔥 Add this line
-  },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
+  }
 });
